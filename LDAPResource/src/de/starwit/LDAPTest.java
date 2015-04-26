6ffe74861068b7fb2f5d58ee365be774374b3ff2
ztarbug <kronprinzfriedrich@gmx.de>
2015-04-26 22:28:37 +0200

    some code cleaning to make checkstyle happy

--- LDAPResource/src/de/starwit/LDAPTest.java
+++ LDAPResource/src/de/starwit/LDAPTest.java
@@ -16,49 +16,53 @@ import javax.naming.directory.DirContext;
 import javax.naming.directory.InitialDirContext;
 import javax.naming.directory.SearchControls;
 import javax.naming.directory.SearchResult;
-import javax.naming.ldap.InitialLdapContext;
-import javax.naming.ldap.LdapContext;
-import javax.print.AttributeException;
 
+/**
+ *
+ * @author Markus Zarbock
+ *
+ * Some command line stuff to test LDAP connections.
+ *
+ */
 public class LDAPTest {
 
 	public static void main(String[] args) throws NamingException, IOException {
-		
+
 		Hashtable<Object, Object> env = new Hashtable<Object, Object>();
-		
-		env.put(Context.INITIAL_CONTEXT_FACTORY,"com.sun.jndi.ldap.LdapCtxFactory");
-		env.put(Context.PROVIDER_URL,"ldap://127.0.0.1:10389");
+
+		env.put(Context.INITIAL_CONTEXT_FACTORY, "com.sun.jndi.ldap.LdapCtxFactory");
+		env.put(Context.PROVIDER_URL, "ldap://127.0.0.1:10389");
 		env.put(Context.SECURITY_AUTHENTICATION, "simple");
 		env.put(Context.SECURITY_PRINCIPAL, "uid=admin,ou=system");
 		env.put(Context.SECURITY_CREDENTIALS, "secret");
 		env.put("java.naming.ldap.attributes.binary", "jpegPhoto");
-		
+
 		DirContext ctx = new InitialDirContext(env);
 		String base = "ou=users,dc=starwit,dc=de";
-		
+
         SearchControls sc = new SearchControls();
         sc.setSearchScope(SearchControls.SUBTREE_SCOPE);
         sc.setReturningObjFlag(true);
 
         String filter = "(&(objectclass=person)(uid=fe_user*))";
         NamingEnumeration<SearchResult> results = ctx.search(base, filter, sc);
-        
+
         while (results.hasMoreElements()) {
 			SearchResult sr = (SearchResult) results.nextElement();
 			Attributes attrs = sr.getAttributes();
-			
+
 			NamingEnumeration<? extends Attribute> attributeNames = attrs.getAll();
 			while (attributeNames.hasMoreElements()) {
 				Attribute attribute = (Attribute) attributeNames.nextElement();
 				System.out.println(attribute.getID() + " : " + attribute.get(0));
 				System.out.println(attribute.get().getClass().getCanonicalName());
 			}
-			
+
 			Attribute attr = attrs.get("jpegPhoto");
-			byte[] content = (byte[])attr.get();
+			byte[] content = (byte[]) attr.get();
 			System.out.println("Size " + content.length);
 
-			BufferedImage image = ImageIO.read( new ByteArrayInputStream( content) );
+			BufferedImage image = ImageIO.read(new ByteArrayInputStream(content));
 			ImageIO.write(image, "PNG", new File("test.png"));
 			System.out.println("++++++++++++++++++++++++++++++++++++++++++++++");
 		}
--- LDAPResource/src/de/starwit/tomcat/LdapContextFactory.java
+++ LDAPResource/src/de/starwit/tomcat/LdapContextFactory.java
@@ -11,48 +11,57 @@ import javax.naming.ldap.InitialLdapContext;
 import javax.naming.ldap.LdapContext;
 import javax.naming.spi.ObjectFactory;
 
+/**
+ *
+ * @author markus
+ *
+ * Simple factory for creating an LDAPContext. Used as a Tomcat resource.
+ *
+ */
 public class LdapContextFactory implements ObjectFactory {
 
-	public Object getObjectInstance(Object obj, Name name, Context nameCtx, Hashtable<?, ?> environment) throws Exception {
+	public Object getObjectInstance(final Object obj, final Name name,
+			final Context nameCtx, final Hashtable<?, ?> environment)
+			throws Exception {
 
-        Hashtable<Object, Object> env = new Hashtable<Object, Object>();
-        Reference reference = (Reference) obj;
-        Enumeration<RefAddr> references = reference.getAll();
+		Hashtable<Object, Object> env = new Hashtable<Object, Object>();
+		Reference reference = (Reference) obj;
+		Enumeration<RefAddr> references = reference.getAll();
 
-        while (references.hasMoreElements()) {
+		while (references.hasMoreElements()) {
 
-            RefAddr address = references.nextElement();
-            String type = address.getType();
-            String content = (String) address.getContent();
+			RefAddr address = references.nextElement();
+			String type = address.getType();
+			String content = (String) address.getContent();
 
-            switch (type) {
+			switch (type) {
 
-            case Context.INITIAL_CONTEXT_FACTORY:
-                env.put(Context.INITIAL_CONTEXT_FACTORY, content);
-                break;
+			case Context.INITIAL_CONTEXT_FACTORY:
+				env.put(Context.INITIAL_CONTEXT_FACTORY, content);
+				break;
 
-            case Context.PROVIDER_URL:
-                env.put(Context.PROVIDER_URL, content);
-                break;
+			case Context.PROVIDER_URL:
+				env.put(Context.PROVIDER_URL, content);
+				break;
 
-            case Context.SECURITY_AUTHENTICATION:
-                env.put(Context.SECURITY_AUTHENTICATION, content);
-                break;
+			case Context.SECURITY_AUTHENTICATION:
+				env.put(Context.SECURITY_AUTHENTICATION, content);
+				break;
 
-            case Context.SECURITY_PRINCIPAL:
-                env.put(Context.SECURITY_PRINCIPAL, content);
-                break;
+			case Context.SECURITY_PRINCIPAL:
+				env.put(Context.SECURITY_PRINCIPAL, content);
+				break;
 
-            case Context.SECURITY_CREDENTIALS:
-                env.put(Context.SECURITY_CREDENTIALS, content);
-                break;
+			case Context.SECURITY_CREDENTIALS:
+				env.put(Context.SECURITY_CREDENTIALS, content);
+				break;
 
-            default:
-                break;
-            }
-        }
+			default:
+				break;
+			}
+		}
 
-        LdapContext context = new InitialLdapContext(env, null);
-        return context;
-    }
+		LdapContext context = new InitialLdapContext(env, null);
+		return context;
+	}
 }
